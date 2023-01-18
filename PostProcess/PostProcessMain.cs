ccfa4a5a0de141abae0c3d614c330ae6d7276b62
Fewdy <dipzei.vibes@gmail.com>
2023-01-18 01:33:30 -0500

    Removed Unnecessary Stuff

--- PostProcess/PostProcessMain.cs
+++ PostProcess/PostProcessMain.cs
@@ -11,15 +11,13 @@ using VRC;
 using VRC.SDKBase;
 using UnityEngine.Rendering.PostProcessing;
 
-namespace PostProcess
+namespace PostProcessingDisabler
 {
-    public class PostProcessing : MelonMod
+    public class PostProcessingDisabler : MelonMod
     {
         private static MethodInfo s_joinMethod { get; set; }
         private delegate IntPtr userJoined(IntPtr _instance, IntPtr _user, IntPtr _methodinfo);
         private static userJoined s_userJoined;
-        public VRCPlayer vrcPlayer;
-        public VRC.Player player;
         public static PostProcessVolume[] s_PostProcess { get; set; }
 
         public override void OnApplicationStart()
@@ -60,7 +58,7 @@ namespace PostProcess
                     if (mt[j].ReadAsObject().ToString().Contains("OnPlayerJoin"))
                     {
                         var methodPointer = *(IntPtr*)(IntPtr)UnhollowerBaseLib.UnhollowerUtils.GetIl2CppMethodInfoPointerFieldForGeneratedMethod(methodInfos[i]).GetValue(null);
-                        MelonUtils.NativeHookAttach((IntPtr)(&methodPointer), typeof(PostProcessing).GetMethod(nameof(OnJoin), BindingFlags.Static | BindingFlags.NonPublic)!.MethodHandle.GetFunctionPointer());
+                        MelonUtils.NativeHookAttach((IntPtr)(&methodPointer), typeof(PostProcessingDisabler).GetMethod(nameof(OnJoin), BindingFlags.Static | BindingFlags.NonPublic)!.MethodHandle.GetFunctionPointer());
                         s_userJoined = Marshal.GetDelegateForFunctionPointer<userJoined>(methodPointer);
                     }
                 }
